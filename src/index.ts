23d360b0a89403a7b52c961f596273a5be461090
cyan <68551684+CyanChanges@users.noreply.github.com>
2023-07-23 21:10:10 +0800

    chore: bump version feat: auto-update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { Context, Schema } from 'koishi'
+import type {} from 'koishi-plugin-k2s'
 
 export const name = 'systools2'
 
-export interface Config {}
+export interface Config {
+}
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.using(['k2s'], (ctx: Context) => {
+    ctx.k2s.ensureEnvironment()
+  })
 }
